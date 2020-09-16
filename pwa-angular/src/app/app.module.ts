afe52e1b15ce2e3761fb6693001185a998f78f53
cartory <xXcartoryXx@gmail.com>
2020-09-16 12:28:02 -0400

    components added

--- pwa-angular/src/app/app.module.ts
+++ pwa-angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VotersComponent } from './components/voters/voters.component';
+import { ResultsComponent } from './components/results/results.component';
+import { CandidatesComponent } from './components/candidates/candidates.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VotersComponent,
+    ResultsComponent,
+    CandidatesComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
