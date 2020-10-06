8afa4c4e523b472f1b74b2468675af94abbea508
cartory <xXcartoryXx@gmail.com>
2020-10-05 23:09:51 -0400

    post

--- votercountapp/lib/src/providers/act.provider.dart
+++ votercountapp/lib/src/providers/act.provider.dart
@@ -8,7 +8,7 @@ import 'package:votercountapp/src/models/env.dart' as env;
 class ActProvider {
   static store({Act act, List<List> voters}) async {
     try {
-      Map<String, dynamic> tmp = Map.fromIterable(
+      Map tmp = Map.fromIterable(
         voters,
         key: (cols) => cols[0],
         value: (cols) => "${cols[1]},${cols[2]}",
@@ -16,11 +16,12 @@ class ActProvider {
 
       final res = await http.post(
         "${env.uri}/tables",
-        body: <String, dynamic>{
+        body: {
           "acta": act.toRawJson(),
           "votos": json.encode(tmp),
         },
       );
+      
       print(res);
     } catch (e) {
       print(e);
