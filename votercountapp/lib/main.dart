fbf68fc50bd95dbf171b11b50975d004b7796fc6
cartory <xXcartoryXx@gmail.com>
2020-10-07 09:34:20 -0400

    device orientation

--- votercountapp/lib/main.dart
+++ votercountapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:votercountapp/src/views/auth/login.page.dart';
 import 'package:votercountapp/src/views/auth/register.page.dart';
 import 'package:votercountapp/src/views/home/home.page.dart';
@@ -11,15 +12,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+    
     return MaterialApp(
       title: 'Elecciones4SW',
       debugShowCheckedModeBanner: false,
       initialRoute: LoginPage.route,
       theme: ThemeData.light(),
       routes: {
-        HomePage.route        : (_) => HomePage(),
-        LoginPage.route       : (_) => LoginPage(),
-        RegisterPage.route    : (_) => RegisterPage(),
+        HomePage.route: (_) => HomePage(),
+        LoginPage.route: (_) => LoginPage(),
+        RegisterPage.route: (_) => RegisterPage(),
       },
     );
   }
