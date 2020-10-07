a13e897c2b2dfe0f35f47ecb57e533b1a1980d5f
cartory <xXcartoryXx@gmail.com>
2020-10-07 12:50:38 -0400

    final seeders

--- backend/database/database.seeder.js
+++ backend/database/database.seeder.js
@@ -1,19 +1,30 @@
+const sequelize = require("../src/database/sequelize");
+
 const { PartySeeder } = require("./seeders/party.seeder");
 const { LocationSeeder } = require("./seeders/location.seeder");
-const sequelize = require("../src/database/sequelize");
-const { PersonSeeder } = require("./seeders/person.seeder");
+const { seedVoters } = require("./seeders/voter.seeder");
+const { seed_votes } = require("./seeders/table-vote.seeder");
 
 function authDB() {
     sequelize
         .authenticate()
         .then().catch();
 }
+
 async function seedDB() {
-    authDB();
-    await PartySeeder.seed();
-    await LocationSeeder.seed();
+    await sequelize.authenticate();
+    await seed_votes();
+    // await PartySeeder.seed();
+    // await LocationSeeder.seed();
     // await PersonSeeder.seed_ADN_politics();
+    // await seedVoters({ number: 1000 });
+    // await seedVoters({ number: 100, delegate: true });
+    // await seedVoters({ number: 100, candidate: true });
+    // await seedVoters({ number: 100, president: true });
+    // await seedVoters({ number: 100, vowel: true });
+    // await seedVoters({ number: 100, secretary: true });
 }
 
-seedDB();
-console.log(`\x1b[32mDB DATABASE SEEDED!!\x1b[0m`);
+seedDB().then(() => {
+    console.log(`\x1b[32mDB DATABASE SEEDED!!\x1b[0m`);
+});
